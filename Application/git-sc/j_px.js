a2fa96d27694af2cf24943e797cd815fbb54e5a0
dogcolley <jinji0326@naver.com>
2019-12-01 16:24:34 +0900

    git hub 잔디 업데이트 / todomylist

---
+++ Application/git-sc/j_px.js
@@ -0,0 +1,38 @@
+const make_px = () =>{
+    
+    const px_arry = new Array();
+
+    px_arry[0] = new Array; 
+    px_arry[1] = new Array; 
+    px_arry[2] = new Array; 
+    px_arry[3] = new Array; 
+    px_arry[4] = new Array; 
+    px_arry[5] = new Array; 
+
+    //console.log(px_arry);  
+    //alert('px _ test for es6');
+
+}
+
+make_px();
+
+/*
+document.querySelector('body').innerHTML('p')
+document.querySelector('div').dataset.productId
+document.getElementsByTagName
+document.querySelector
+document.querySelectorAll
+
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    // start ...
+})
+
+/*
+const evt = new CustomEvent('@click')
+document.dispatchEvent(evt)
+*/
+
+
+

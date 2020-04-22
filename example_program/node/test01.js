6ec84b4603b2615c701e1643cd9cc2bd5b1463fd
dogcolley <jinji0326@naver.com>
2020-04-22 21:47:57 +0900

    노드 모듈 테스트 하기

---
+++ example_program/node/test01.js
@@ -0,0 +1,5 @@
+
+const moduleA = require('./testModule');
+
+moduleA.console2('test');
+moduleA.console2(moduleA.sum(1,2));
---
+++ example_program/node/testModule.js
@@ -0,0 +1,7 @@
+exports.console2 = function(text){
+    console.log(text);
+}
+
+exports.sum = function(a,b){
+    return a + b;
+}

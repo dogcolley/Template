080848b2df915a4d624e263b4b51f0b137795d2b
dogcolley <jinji0326@naver.com>
2020-07-13 10:40:09 +0900

    this 란

--- example_program/ts/basic/dist/this.js
+++ example_program/ts/basic/dist/this.js
@@ -1,5 +1,4 @@
 //this의 개념을 모르는 멍청한 개밥이여..
-
 var foo = function () {
     console.log(this);
 }
@@ -33,7 +32,54 @@ function range(){
         console.log(this.value);
     }
     range2();
+
+    var test = 1;
+    console.log(test);
+    const a = () =>{
+        this.test = 2;
+        console.log('화살표의 a this',this);
+        console.log(this.test);
+        var test2 = 2;
+        this.test2 = 2;
+        const b = () =>{
+            console.log('화살표의 b this',this);
+        }
+        b();
+        
+        function c (){
+            console.log('그냥함수 C this',this);
+        }
+        c();
+
+    }
+    a();
 }
 
-range();
+//range();
 
+class testB {
+
+    constructor(){
+        this.a =1; 
+    }
+
+    test (){
+        console.log('class 의 this는',this);
+        thisConsole();
+        thisConsole2();
+    }
+
+}
+
+function thisConsole (){
+    console.log('나는 최상위 this를 호출한다',this)
+}
+
+const thisConsole2 = () =>{
+    console.log('나는 나 자신을 this를 호출한다.',this);
+}
+const tests = new testB();
+tests.test();
+
+
+console.log('node의 this는',this);
